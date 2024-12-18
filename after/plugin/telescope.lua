21823281fe1a32cbe5ec07d77ef3598e61e858f3
Tiberiu <tiberiuiurco@gmail.com>
2024-12-19 06:31:09 +0900

    Telescope additions

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,4 @@
+require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>o', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
@@ -6,3 +7,15 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Telescope resume last search' })
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep cursor string' })
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics buffer' })
+vim.keymap.set("n", "<leader>en", function()
+  builtin.find_files({
+    prompt_title = 'Neovim Config',
+    cwd = vim.fn.stdpath('config')
+  })
+end, { desc = 'Telescope find nvim config' })
+vim.keymap.set("n", "<leader>ep", function()
+  builtin.find_files({
+    prompt_title = 'Neovim Packages',
+    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+  })
+end, { desc = 'Telescope find nvim config' })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,19 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function()
+    require('telescope').setup {
+      pickers = {
+        find_files = {
+          theme = "ivy"
+        },
+      },
+      extensions = {
+        fzf = {}
+      }
     }
+  end
+}
